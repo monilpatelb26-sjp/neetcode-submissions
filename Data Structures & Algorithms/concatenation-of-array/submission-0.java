80e4fd3116cc7d81587db031e363c7854115e487
monilpatelb26-sjp <monilpatelb31@gmail.com>
2026-07-01 12:03:47 +0530

    Add: concatenation-of-array - submission-0

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int ans[] = new int[2*nums.length];
+        int k = 0;
+        for(int i = 0;i<2;i++){
+            for(int j=0;j<nums.length;j++){
+                 ans[k] = nums[j];
+                 k++;
+            }
+        }
+        return ans;
+    }
+}
